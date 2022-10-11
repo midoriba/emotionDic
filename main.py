43ccb3b2edfa3ba9dc6b4991aa4be226ad24bc98
midorikawa <48616869+midoriba@users.noreply.github.com>
2022-10-11 11:22:24 +0900

    コーパスの各発話を、speakerとcontentsをキーに持つ辞書型で登録するように変更

--- main.py
+++ main.py
@@ -4,25 +4,30 @@ def read(path: str):
     with open(path, encoding='utf-8', mode='r') as f:
         txt = f.readlines()
         ret = []
-        tmp = ''
+        tmp = {'speaker':'', 'content':''}
+        cnt = 0
         for iraw in txt:
             i = iraw.strip()
             if(i[0] == '＠'):
                 continue
             elif('：' in i):
-                if(tmp != ''):
-                    tmp = re.sub('（.+?）', '', tmp)
-                    tmp = re.sub('＜.+?＞', '', tmp)
+                if(tmp['content'] != ''):
+                    tmp['content'] = re.sub('（.+?）', '', tmp['content'])
+                    tmp['content'] = re.sub('＜.+?＞', '', tmp['content'])
                     ret.append(tmp)
-                tmp = re.sub('.+：', '', i)
+                    tmp = {'speaker':'', 'content':''}
+                splited_i = i.split('：')
+                if(len(splited_i) != 2):
+                    print(f'確認してください: "{i}"')
+                else:
+                    tmp['speaker'], tmp['content'] = splited_i
             else:
-                tmp += i
-        tmp = re.sub('（.+?）', '', tmp)
-        tmp = re.sub('＜.+?＞', '', tmp)
+                tmp['content'] += i
+        tmp['content'] = re.sub('（.+?）', '', tmp['content'])
+        tmp['content'] = re.sub('＜.+?＞', '', tmp['content'])
         ret.append(tmp)
         return ret
 
 if(__name__ == '__main__'):
     res = read('data/nucc/data001.txt')
-    for i in res:
-        print(i)
+    print(res)
