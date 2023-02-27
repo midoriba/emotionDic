f9da36a8f10dfc66a50ece13e90827675b50afbe
midoriba <48616869+midoriba@users.noreply.github.com>
2023-02-27 14:00:57 +0900

    否定語か逆接の接続助詞が含まれる文節を記録する配列を追記

--- src/modules/sentence.py
+++ src/modules/sentence.py
@@ -1,8 +1,14 @@
 import CaboCha
 import json
+from pathlib import Path
 
 
 parser = CaboCha.Parser('-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd')
+project_dir = Path(__file__).parent.parent.parent
+with open(project_dir.joinpath('conf', 'connectorword.json'), encoding='utf-8', mode='r') as f:
+    connector_dic = json.load(f)
+with open(project_dir.joinpath('conf', 'negativeword.json'), encoding='utf-8', mode='r') as f:
+    negative_dic = json.load(f)
 
 
 def read_sentence(text):
@@ -23,10 +29,17 @@ def read_sentence(text):
         new_sentence.base_form_words.append(feature[6])
         new_sentence.word_category.append(feature[0])
         new_sentence.sub_word_category.append(feature[1])
+        new_sentence.bunsetsu_number.append(new_sentence.bunsetsu_size)
+        if(feature[1] == '接続助詞' and feature[6] in connector_dic['接続助詞']['逆接']):
+            new_sentence.reverse_dependency[-1] = True
+        if(feature[0] == '助動詞' and feature[6] in negative_dic['助動詞']):
+            new_sentence.negative[-1] = True
         if(token.chunk is not None):
             new_sentence.bunsetsu_size += 1
             new_sentence.bunsetsu_head.append(index)
             new_sentence.dependency.append(token.chunk.link)
+            new_sentence.reverse_dependency.append(False)
+            new_sentence.negative.append(False)
     return new_sentence
 
 
@@ -36,6 +49,7 @@ def read_json(json_text):
     new_sentence.text = dic['text']
     new_sentence.raw_words = dic['raw_words']
     new_sentence.base_form_words = dic['base_form_words']
+    new_sentence.bunsetsu_number = dic['bunsetsu_number']
     new_sentence.length = dic['length']
     new_sentence.bunsetsu_size = dic['bunsetsu_size']
     new_sentence.bunsetsu_head = dic['bunsetsu_head']
@@ -49,10 +63,13 @@ class Sentence:
         self.text = ''
         self.raw_words = list()
         self.base_form_words = list()
+        self.bunsetsu_number = list()
         self.length = 0
         self.bunsetsu_size = 0
         self.bunsetsu_head = list()
         self.dependency = list()
+        self.reverse_dependency = list()
+        self.negative = list()
         self.word_category = list()
         self.sub_word_category = list()
     def describe(self):
@@ -65,11 +82,12 @@ class Sentence:
 
 
 def main():
-    text = "え？私、昨日実は電話来たとき寝てたんだよね。"
+    text = "売ってたのにあまり綺麗じゃなかった。"
     s = read_sentence(text)
     json_s = s.to_json()
     print(json_s)
     print(parser.parseToString(text))
+    print(negative_dic['助動詞'])
 
 if(__name__ == '__main__'):
     main()
